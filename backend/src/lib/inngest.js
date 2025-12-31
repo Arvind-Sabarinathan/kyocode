da818cc7f3be08dc95b41ec1de5301fdde5fae85
Arvind-Sabarinathan <arvind.personal2004@gmail.com>
2026-01-01 02:05:49 +0530

    feat(backend): sync Clerk user create and delete events with DB using Inngest

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,37 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import User from "../models/user.model.js";
+
+export const inngest = new Inngest({ id: "kyocode" });
+
+const syncUserCreated = inngest.createFunction(
+  { id: "sync-user-created" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
+
+    await User.create({
+      clerkID: id,
+      name: `${first_name || ""} ${last_name || ""}`,
+      email: email_addresses[0].email_address,
+      profileImage: image_url,
+    });
+  }
+);
+
+const syncUserDeleted = inngest.createFunction(
+  { id: "sync-user-deleted" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id } = event.data;
+
+    await User.deleteOne({ clerkID: id });
+  }
+);
+
+export const functions = [syncUserCreated, syncUserDeleted];
